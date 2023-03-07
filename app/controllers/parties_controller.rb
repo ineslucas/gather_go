0283df37f03d0ad4a39b62caf2d340aba6229f07
Joao Gouveia <joaogouveia.work@gmail.com>
2023-03-07 15:24:29 +0000

    branch on hold

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,2 +1,6 @@
 class PartiesController < ApplicationController
+  def show
+    @party = Party.find(params[:id])
+    @party = Party.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :parties, only: [:show]
+  # get 'parties/:id', to: 'parties#show'
 end
