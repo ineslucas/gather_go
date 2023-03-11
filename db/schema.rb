8e924e8001f1568eac06e36c324ad8739dc5e40e
Ines Lucas <lucasinesmaria@gmail.com>
2023-03-11 15:11:42 +0000

    made changes to edibles model + created some example seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_103125) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_11_142929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_103125) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "ingredients"
+    t.text "instructions"
+    t.text "description"
   end
 
   create_table "parties", force: :cascade do |t|
