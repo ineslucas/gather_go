02443ac0696413e8802243962a20c5e1f60175d1
Ines Lucas <lucasinesmaria@gmail.com>
2023-03-12 13:07:47 +0000

    changes in progress cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :bookings
+  has_one_attached :profile_picture
+  mount_uploader :profile_picture, ProfilePictureUploader
 end
